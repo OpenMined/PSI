7b147b0ba6f3b5e3e9db64b7121ef25ae111d81f
Nick Angelou <nick@morfix.io>
2020-05-06 12:38:05 -0500

    feat: rollup config outputs

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 // import { terser } from 'rollup-plugin-terser'
 
 export default [
+  /////////////////
+  // Client
+  /////////////////
   {
     input: 'psi_cardinality/javascript/src/index_client_wasm.js',
     output: {
       file: 'psi_cardinality/javascript/dist/client/wasm/index.js',
       sourcemap: true,
       format: 'umd',
-      name: 'psi.js'
+      name: 'PSICardinality'
       // plugins: [terser()]
     }
   },
@@ -17,7 +20,53 @@ export default [
       file: 'psi_cardinality/javascript/dist/client/js/index.js',
       sourcemap: true,
       format: 'umd',
-      name: 'psi.js'
+      name: 'PSICardinality'
+      // plugins: [terser()]
+    }
+  },
+  /////////////////
+  // Server
+  /////////////////
+  {
+    input: 'psi_cardinality/javascript/src/index_server_wasm.js',
+    output: {
+      file: 'psi_cardinality/javascript/dist/server/wasm/index.js',
+      sourcemap: true,
+      format: 'umd',
+      name: 'PSICardinality'
+      // plugins: [terser()]
+    }
+  },
+  {
+    input: 'psi_cardinality/javascript/src/index_server_js.js',
+    output: {
+      file: 'psi_cardinality/javascript/dist/server/js/index.js',
+      sourcemap: true,
+      format: 'umd',
+      name: 'PSICardinality'
+      // plugins: [terser()]
+    }
+  },
+  /////////////////
+  // Combined
+  /////////////////
+  {
+    input: 'psi_cardinality/javascript/src/index_combined_wasm.js',
+    output: {
+      file: 'psi_cardinality/javascript/dist/combined/wasm/index.js',
+      sourcemap: true,
+      format: 'umd',
+      name: 'PSICardinality'
+      // plugins: [terser()]
+    }
+  },
+  {
+    input: 'psi_cardinality/javascript/src/index_combined_js.js',
+    output: {
+      file: 'psi_cardinality/javascript/dist/combined/js/index.js',
+      sourcemap: true,
+      format: 'umd',
+      name: 'PSICardinality'
       // plugins: [terser()]
     }
   }
