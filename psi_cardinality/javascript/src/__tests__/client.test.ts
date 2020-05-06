807b9be85539eae187007d6c80e3ac15e586aacd
willclarktech <willclarktech@users.noreply.github.com>
2020-05-06 16:18:31 +0100

    test: improve ts tests

--- psi_cardinality/javascript/src/__tests__/client.test.ts
+++ psi_cardinality/javascript/src/__tests__/client.test.ts
@@ -1,51 +1,77 @@
 import PSICardinality from '../index_combined_wasm'
-import { Client } from '../implementation/client'
+import { ERROR_INSTANCE_DELETED } from '../implementation/constants'
 
 describe('PSI Client', () => {
-  let client: Client
+  test('It should throw if deleted twice', async () => {
+    const client = await PSICardinality.client.create()
 
-  beforeAll(async () => {
-    client = await PSICardinality.client.create()
+    client.delete()
+
+    expect(client.delete).toThrowError(ERROR_INSTANCE_DELETED)
   })
 
-  test('It should delete its instance', async () => {
-    const client2 = await PSICardinality.client.create()
-    const spyOn = jest.spyOn(client2, 'delete')
-    client2.delete()
-    expect(spyOn).toHaveBeenCalled()
-  })
-  test('It should create a request', () => {
+  test('It should create a request', async () => {
+    const client = await PSICardinality.client.create()
     const numClientElements = 100
     const clientInputs = Array.from(
       { length: numClientElements },
       (_, i) => `Element ${i}`
     )
+
     const request = client.createRequest(clientInputs)
-    expect(typeof request).toBe('string')
+    const parsed = JSON.parse(request)
+
+    expect(parsed.length).toStrictEqual(numClientElements)
   })
-  // test('It should fail to create a request', () => {
-  //   const numClientElements = 10
-  //   const clientInputs = Array.from({ length: numClientElements }, (_, i) => i)
-  //   const spyOn = jest.spyOn(client, 'createRequest')
-  //   expect(() => client.createRequest(clientInputs)).toThrow()
-  //   expect(spyOn).toHaveBeenCalled()
-  // })
-  test('It should process a response', () => {
+
+  test('It should throw if attempting to create a request after deletion', async () => {
+    const client = await PSICardinality.client.create()
+    const numClientElements = 100
+    const clientInputs = Array.from(
+      { length: numClientElements },
+      (_, i) => `Element ${i}`
+    )
+
+    client.delete()
+
+    expect(client.createRequest.bind(client, clientInputs)).toThrowError(
+      ERROR_INSTANCE_DELETED
+    )
+  })
+
+  test('It should process a response', async () => {
+    const client = await PSICardinality.client.create()
     const serverSetup =
       '{"num_hash_functions":14,"bits":"1PnolAVduzqn+tXnoeyZ0ztdnwvAidt0U7gc2rWCtP5i6BDtSUydixXwGHP2mVAZ8PCNqGuTWUQO5VB0OEHTH8Cr1JhLxBDHRM4fAPEUvjjkBPNpsPRRtqzN2fW8y7rcANEwaRyInPsw1t4KYB0Q5vdy38VlPB1/Bz/adfGoGnXQ4fPNI/PN5s+zLcPev9odjAWyUQms8bvPHHsL2uyS7Uu5LRtOfdfYX5zWilxzNXECrabnnUnMqD5NH8c7oreTCWxwRQfYMxhf5K/EZTiTHphCi9O5Ey4ahwvNvykbvXdHQ7CDnLTKmomN6su5BrUt"}'
     const serverResponse =
       '["AiB2zX47wIFkO0VkHPiX4B2RPbzb2xre1/rcH3mARf6A","Aix7yJwCoTbF1RDFgLT7ntQ+DCdsxtQB5VKemNSbs95K","AlqATF16utJn0b2/mT4F/Tnxd7bQJUBc2em7H4OGkj7v","AnBfbcXx7YZ42/0zuIWyw0xECaf2bzlPWPe1TpD8LC1G","ArJt9KivSQERWNCAyUt1dAz4B2eud0qxnu206TJ0FxoT","AxfuH374m21PsyxFKQY9J2Seq/abvQv1cqDse8+f1DCv","AyHI21aQJyaa9HM3rPhhGt99iKHNg5wnwVeGwcmGggun","A0lFTmKUvyD7JzzR9J+syz+myC2metGmKb34FS4AkDQx","A2XvsIdnkPF6PJ6tPRI2LTdxguChs83Vd1VTM3DQXkdX","A606MaSef27r/KxUa1iRVWaX7lZqioxfz2iFpOR+6Yts"]'
 
-    const spyOn = jest.spyOn(client, 'processResponse')
     const intersection = client.processResponse(serverSetup, serverResponse)
-    expect(spyOn).toHaveBeenCalled()
-    expect(typeof intersection).toBe('number')
+
+    expect(intersection).toStrictEqual(0)
   })
-  test('It should fail to process a response', () => {
+
+  test('It should throw if attempting to process a response after deletion', async () => {
+    const client = await PSICardinality.client.create()
+    const serverSetup =
+      '{"num_hash_functions":14,"bits":"1PnolAVduzqn+tXnoeyZ0ztdnwvAidt0U7gc2rWCtP5i6BDtSUydixXwGHP2mVAZ8PCNqGuTWUQO5VB0OEHTH8Cr1JhLxBDHRM4fAPEUvjjkBPNpsPRRtqzN2fW8y7rcANEwaRyInPsw1t4KYB0Q5vdy38VlPB1/Bz/adfGoGnXQ4fPNI/PN5s+zLcPev9odjAWyUQms8bvPHHsL2uyS7Uu5LRtOfdfYX5zWilxzNXECrabnnUnMqD5NH8c7oreTCWxwRQfYMxhf5K/EZTiTHphCi9O5Ey4ahwvNvykbvXdHQ7CDnLTKmomN6su5BrUt"}'
+    const serverResponse =
+      '["AiB2zX47wIFkO0VkHPiX4B2RPbzb2xre1/rcH3mARf6A","Aix7yJwCoTbF1RDFgLT7ntQ+DCdsxtQB5VKemNSbs95K","AlqATF16utJn0b2/mT4F/Tnxd7bQJUBc2em7H4OGkj7v","AnBfbcXx7YZ42/0zuIWyw0xECaf2bzlPWPe1TpD8LC1G","ArJt9KivSQERWNCAyUt1dAz4B2eud0qxnu206TJ0FxoT","AxfuH374m21PsyxFKQY9J2Seq/abvQv1cqDse8+f1DCv","AyHI21aQJyaa9HM3rPhhGt99iKHNg5wnwVeGwcmGggun","A0lFTmKUvyD7JzzR9J+syz+myC2metGmKb34FS4AkDQx","A2XvsIdnkPF6PJ6tPRI2LTdxguChs83Vd1VTM3DQXkdX","A606MaSef27r/KxUa1iRVWaX7lZqioxfz2iFpOR+6Yts"]'
+
+    client.delete()
+
+    expect(
+      client.processResponse.bind(client, serverSetup, serverResponse)
+    ).toThrowError(ERROR_INSTANCE_DELETED)
+  })
+
+  test('It should fail to process a response', async () => {
+    const client = await PSICardinality.client.create()
     const serverSetup = 'invalid'
     const serverResponse = 'invalid'
-    const spyOn = jest.spyOn(client, 'processResponse')
-    expect(() => client.processResponse(serverSetup, serverResponse)).toThrow()
-    expect(spyOn).toHaveBeenCalled()
+
+    expect(
+      client.processResponse.bind(client, serverSetup, serverResponse)
+    ).toThrow()
   })
 })
--- psi_cardinality/javascript/src/__tests__/server.test.ts
+++ psi_cardinality/javascript/src/__tests__/server.test.ts
@@ -1,78 +1,109 @@
 import PSICardinality from '../index_combined_wasm'
-import { Server } from '../implementation/server'
+import { ERROR_INSTANCE_DELETED } from '../implementation/constants'
 
 describe('PSI Server', () => {
-  let server: Server
-
-  beforeAll(async () => {
-    server = await PSICardinality.server.createWithNewKey()
-  })
-
   test('It should create from an existing key', async () => {
-    const server2 = await PSICardinality.server.createWithNewKey()
-    const key = server2.getPrivateKeyBytes()
-    const spyOn = jest.spyOn(PSICardinality.server, 'createFromKey')
-    const server = await PSICardinality.server.createFromKey(key)
-    expect(spyOn).toHaveBeenCalledWith(key)
-    expect(server).toBeInstanceOf(Object)
-    expect(server.constructor).toBe(Object)
-    expect(server.constructor.name).toBe('Object')
-    expect(server.getPrivateKeyBytes()).toEqual(key)
+    const server1 = await PSICardinality.server.createWithNewKey()
+    const key = server1.getPrivateKeyBytes()
+
+    const server2 = await PSICardinality.server.createFromKey(key)
+
+    expect(server2.getPrivateKeyBytes()).toEqual(key)
   })
+
   test('It should fail to create from an invalid key', async () => {
     const key = Uint8Array.from({ length: 32 })
-    const spyOn = jest.spyOn(PSICardinality.server, 'createFromKey')
     await expect(PSICardinality.server.createFromKey(key)).rejects.toThrow()
-    expect(spyOn).toHaveBeenCalledWith(key)
   })
-  test('It should delete its instance', async () => {
+
+  test('It should throw if deleted twice', async () => {
     const server = await PSICardinality.server.createWithNewKey()
-    const spyOn = jest.spyOn(server, 'delete')
     server.delete()
-    expect(spyOn).toHaveBeenCalled()
+    expect(server.delete).toThrowError(ERROR_INSTANCE_DELETED)
   })
-  test('It should return the private key as a binary array', () => {
-    const spyOn = jest.spyOn(server, 'getPrivateKeyBytes')
+
+  test('It should return the private key as a binary array', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
+    const keyLength = 32
     const key = server.getPrivateKeyBytes()
-    expect(spyOn).toHaveBeenCalled()
-    expect(key.constructor).toBe(Uint8Array)
+    expect(key.length).toBe(keyLength)
   })
-  test('It should create a setup message', () => {
+
+  test('It should create a setup message', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
     const fpr = 0.001
     const numClientElements = 10
     const serverInputs = Array.from({ length: 100 }, (_, i) => `Element ${i}`)
 
-    const spyOn = jest.spyOn(server, 'createSetupMessage')
     const setup = server.createSetupMessage(
       fpr,
       numClientElements,
       serverInputs
     )
-    expect(spyOn).toHaveBeenCalledWith(fpr, numClientElements, serverInputs)
-    expect(typeof setup).toBe('string')
+    const parsed = JSON.parse(setup)
+
+    expect(typeof parsed.num_hash_functions).toStrictEqual('number')
+    expect(typeof parsed.bits).toStrictEqual('string')
   })
-  test('It should fail to create a setup message', () => {
+
+  test('It should throw if attempting to create a setup message after deletion', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
+    const fpr = 0.001
+    const numClientElements = 10
+    const serverInputs = Array.from({ length: 100 }, (_, i) => `Element ${i}`)
+
+    server.delete()
+
+    expect(
+      server.createSetupMessage.bind(
+        server,
+        fpr,
+        numClientElements,
+        serverInputs
+      )
+    ).toThrowError(ERROR_INSTANCE_DELETED)
+  })
+
+  test('It should fail to create a setup message', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
     const fpr = 0.001
     const numClientElements = -10
     const serverInputs = Array.from({ length: 100 }, (_, i) => `Element ${i}`)
 
-    const spyOn = jest.spyOn(server, 'createSetupMessage')
-    expect(() =>
-      server.createSetupMessage(fpr, numClientElements, serverInputs)
+    expect(
+      server.createSetupMessage.bind(
+        server,
+        fpr,
+        numClientElements,
+        serverInputs
+      )
     ).toThrow()
-    expect(spyOn).toHaveBeenCalledWith(fpr, numClientElements, serverInputs)
   })
-  test('It should create a response to a client request', () => {
+
+  test('It should create a response to a client request', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
     const clientRequest = '["AiHdmxkmF/iOM0fFhny9917QYGcb9jq0GM9mP4L74ecM"]'
-    const spyOn = jest.spyOn(server, 'processRequest')
+
     const response = server.processRequest(clientRequest)
-    expect(spyOn).toHaveBeenCalledWith(clientRequest)
+
     expect(typeof response).toBe('string')
   })
-  test('It should fail to create a response from an invalid client request', () => {
+
+  test('It should throw if attempting to create a response to a client request after deletion', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
+    const clientRequest = '["AiHdmxkmF/iOM0fFhny9917QYGcb9jq0GM9mP4L74ecM"]'
+
+    server.delete()
+
+    expect(server.processRequest.bind(server, clientRequest)).toThrowError(
+      ERROR_INSTANCE_DELETED
+    )
+  })
+
+  test('It should fail to create a response from an invalid client request', async () => {
+    const server = await PSICardinality.server.createWithNewKey()
     const clientRequest = 'invalid request'
-    const spyOn = jest.spyOn(server, 'processRequest')
-    expect(() => server.processRequest(clientRequest)).toThrow()
-    expect(spyOn).toHaveBeenCalledWith(clientRequest)
+
+    expect(server.processRequest.bind(server, clientRequest)).toThrow()
   })
 })
