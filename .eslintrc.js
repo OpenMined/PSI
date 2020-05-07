0c2a509a468335f2ee5435d32e4e4f7be3a16bb0
willclarktech <willclarktech@users.noreply.github.com>
2020-05-07 16:46:09 +0100

    chore: update eslint parser ecmaVersion

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     'plugin:jest/recommended'
   ],
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2020,
     sourceType: 'module'
   },
   plugins: ['@typescript-eslint', 'jsdoc', 'jest', 'prettier'],
