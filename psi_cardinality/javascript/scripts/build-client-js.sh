7a7ea9fe6d2b8b63b0120b5ee857351fd4e65492
Phillipp Schoppmann <schoppmp@users.noreply.github.com>
2020-05-13 08:30:55 -0700

    Refactor third-party dependencies (#37)

--- psi_cardinality/javascript/scripts/build-client-js.sh
+++ psi_cardinality/javascript/scripts/build-client-js.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # Source emsdk
-source ./submodules/emsdk/emsdk_env.sh
+source ./third_party/emsdk/emsdk_env.sh
 
 # Build the JS variant
 bazel build -c opt //psi_cardinality/javascript:psi_cardinality_client_js.js --config=js
