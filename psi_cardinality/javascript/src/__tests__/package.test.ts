7b38befa632de51bd8efde142cb53910a4f237c7
Nick Angelou <nick@morfix.io>
2020-05-18 17:52:59 -0500

    chore: update test

--- psi_cardinality/javascript/src/__tests__/package.test.ts
+++ psi_cardinality/javascript/src/__tests__/package.test.ts
@@ -3,7 +3,7 @@ import PSICardinality from '../index_combined_wasm'
 describe('PSI Package', () => {
   test('It should return the version string', async () => {
     const PSI = await PSICardinality()
-    const version = PSI.package.version
+    const { version } = PSI.package
     expect(typeof version).toBe('string')
   })
 })
