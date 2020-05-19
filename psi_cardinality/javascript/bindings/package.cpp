a703265839c169d606c847b790e9d3d3a5b3ba76
Nick Angelou <nick@morfix.io>
2020-05-19 12:38:24 -0500

    fix: namespace of bindings

--- psi_cardinality/javascript/bindings/package.cpp
+++ psi_cardinality/javascript/bindings/package.cpp
@@ -3,7 +3,7 @@
 #include <emscripten/bind.h>
 
 EMSCRIPTEN_BINDINGS(Package) {
-  emscripten::class_<Package>("Package")
+  emscripten::class_<psi_cardinality::Package>("Package")
       // Using class_function instead of class_property
       // because emscripten cannot understand a static constexpr char[]
       .class_function("version", emscripten::optional_override([]() {
