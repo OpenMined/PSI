8ff864e5b24af467153e1405c3b0a64fdd3fbb0d
Phillipp Schoppmann <schoppmann@informatik.hu-berlin.de>
2020-05-22 18:35:58 +0200

    Remove install scripts

--- .github/workflows/scripts/install_macos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# Download the install script.
-curl -LO https://github.com/bazelbuild/bazel/releases/download/3.1.0/bazel-3.1.0-installer-darwin-x86_64.sh
-
-# Run the installer.
-chmod +x bazel-3.1.0-installer-darwin-x86_64.sh
-sudo ./bazel-3.1.0-installer-darwin-x86_64.sh
