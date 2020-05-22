4ec68b803f76a40b123b625feb4a1a4036a98fca
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-05-22 10:10:16 +0300

    macos: install clang-format

--- .github/workflows/scripts/install_macos.sh
+++ .github/workflows/scripts/install_macos.sh
@@ -7,3 +7,6 @@ curl -LO https://github.com/bazelbuild/bazel/releases/download/3.1.0/bazel-3.1.0
 # Run the installer.
 chmod +x bazel-3.1.0-installer-darwin-x86_64.sh
 sudo ./bazel-3.1.0-installer-darwin-x86_64.sh
+
+# CPP linters
+brew install clang-format
