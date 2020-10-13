7f25e91268a4ae17f79a02abedf1ae8b01f18e65
Nick Angelou <nick.angelou@runpanther.io>
2020-10-13 10:06:54 -0500

    fix: add proto definitions to bundle

--- private_set_intersection/javascript/scripts/postrollup.sh
+++ private_set_intersection/javascript/scripts/postrollup.sh
@@ -7,6 +7,9 @@ cp -rf private_set_intersection/javascript/README.md private_set_intersection/ja
 cp -rf LICENSE private_set_intersection/javascript/dist
 cp -rf CHANGES.md private_set_intersection/javascript/dist
 
+# Need to copy the protobuf definitions manually as rollup doesn't do this
+mkdir -p private_set_intersection/javascript/dist/implementation/proto && cp -rf private_set_intersection/javascript/src/implementation/proto/psi_pb.d.ts private_set_intersection/javascript/dist/implementation/proto
+
 # Change directory into dist and pack to get
 # shorter deep import links.
 # Ex: 
