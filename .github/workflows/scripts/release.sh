becfd602ea9dfdfd3c5f3cf30b97288c5af6a911
Bogdan Cebere <bogdan.cebere@gmail.com>
2020-11-27 08:39:34 +0200

    Update release scripts

--- .github/workflows/scripts/release.sh
+++ .github/workflows/scripts/release.sh
@@ -3,6 +3,5 @@
 python -m pip install --upgrade pip
 pip install setuptools wheel twine auditwheel
 
-pip wheel . -w dist/ --no-deps
-auditwheel repair dist/*.whl --plat $AUDITWHEEL_PLAT
-twine upload --skip-existing wheelhouse/*
+pip wheel . -w wheel/
+twine upload --skip-existing wheel/*
