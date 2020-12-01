a11e39b0a2cdef64c6986490ba30a897eb15a2ff
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-01 12:41:28 +0200

    build a newer GCC for bazel

--- .github/workflows/scripts/release_linux.sh
+++ .github/workflows/scripts/release_linux.sh
@@ -2,10 +2,39 @@
 
 set -e
 
-bazel_version="3.7.1"
-# Install gcc/g++, git, unzip/which (for bazel), and python3
-yum install -y python3 python3-devel gcc gcc-c++ git unzip which
+ARG GCC_VERSION=8.3.0
+ARG GCC_PATH=/usr/local/gcc-$GCC_VERSION
 
+yum -y update && yum -y install \
+    curl \
+    bison \
+    flex \
+    python3 python3-devel \
+    git unzip which \
+    && yum clean all
+
+cd /tmp \
+    && curl -L -o gcc.tar.gz "https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz" \
+    && tar xf gcc.tar.gz \
+    && cd /tmp/gcc-$GCC_VERSION \
+    && contrib/download_prerequisites \
+    && mkdir build \
+    && cd build \
+    && ../configure -v \
+        --build=x86_64-linux-gnu \
+        --host=x86_64-linux-gnu \
+        --target=x86_64-linux-gnu \
+        --prefix=/usr/local/gcc-$GCC_VERSION \
+        --enable-checking=release \
+        --enable-languages=c,c++,fortran \
+        --disable-multilib \
+        --program-suffix=-$GCC_VERSION \
+    && make -j4 \
+    && make install-strip \
+    && cd /tmp \
+    && rm -rf /tmp/gcc-$GCC_VERSION /tmp/gcc.tar.gz
+
+bazel_version="3.7.1"
 # Install Bazel version specified in .bazelversion
 curl -sSOL https://github.com/bazelbuild/bazel/releases/download/${bazel_version}/bazel-${bazel_version}-installer-linux-x86_64.sh
 bash -x -e bazel-${bazel_version}-installer-linux-x86_64.sh
