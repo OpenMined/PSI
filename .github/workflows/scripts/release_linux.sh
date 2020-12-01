9d398516937b20018afed7037ef8f02d2738c00b
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-01 16:13:02 +0200

    rework package upload

--- .github/workflows/scripts/release_linux.sh
+++ .github/workflows/scripts/release_linux.sh
@@ -5,27 +5,24 @@ set -e
 yum -y update
 yum -y install epel-release
 yum --disablerepo="*" --enablerepo="epel" list python3*
-yum --disablerepo="*" --enablerepo="epel" install python3 python3-devel
-curl -O https://bootstrap.pypa.io/get-pip.py
-/usr/bin/python3 get-pip.py
 
 yum -y install \
     curl bison flex \
-    epel-release python python-devel python-pip \
+    epel-release python3 python3-devel python3-pip \
     git unzip which \
     && yum clean all
 
 bazel_version="3.7.1"
 # Install Bazel version specified in .bazelversion
-curl -sSOL https://github.com/bazelbuild/bazel/releases/download/${bazel_version}/bazel-${bazel_version}-installer-linux-x86_64.sh
+curl -sSOLv https://github.com/bazelbuild/bazel/releases/download/${bazel_version}/bazel-${bazel_version}-installer-linux-x86_64.sh
 bash -x -e bazel-${bazel_version}-installer-linux-x86_64.sh
 
 # Python
-python -m pip install --upgrade pip
-pip install -r private_set_intersection/python/requirements_dev.txt
-pip install setuptools wheel twine auditwheel
+python3 -m pip install --upgrade pip
+python3 -m pip install -r private_set_intersection/python/requirements_dev.txt
+python3 -m pip install setuptools wheel twine auditwheel
 
 # Publish
-pip wheel . -w dist/ --no-deps
+python3 -m pip wheel . -w dist/ --no-deps
 auditwheel repair dist/*.whl --plat $AUDITWHEEL_PLAT
 twine upload --skip-existing wheelhouse/*
