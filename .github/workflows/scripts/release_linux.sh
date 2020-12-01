eaee9a1b7d6cdc99c9d9c9538daad809e2736a24
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-01 13:19:04 +0200

    use a precompiled docker image

--- .github/workflows/scripts/release_linux.sh
+++ .github/workflows/scripts/release_linux.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-GCC_VERSION=8.3.0
-GCC_PATH=/usr/local/gcc-$GCC_VERSION
-
 yum -y update && yum -y install \
     curl \
     bison \
@@ -12,27 +9,6 @@ yum -y update && yum -y install \
     python3 python3-devel \
     git unzip which \
     && yum clean all
-
-cd /tmp \
-    && curl -L -o gcc.tar.gz "https://ftp.gnu.org/gnu/gcc/gcc-${GCC_VERSION}/gcc-${GCC_VERSION}.tar.gz" \
-    && tar xf gcc.tar.gz \
-    && cd /tmp/gcc-$GCC_VERSION \
-    && contrib/download_prerequisites \
-    && mkdir build \
-    && cd build \
-    && ../configure -v \
-        --build=x86_64-linux-gnu \
-        --host=x86_64-linux-gnu \
-        --target=x86_64-linux-gnu \
-        --prefix=/usr/local/gcc-$GCC_VERSION \
-        --enable-checking=release \
-        --enable-languages=c,c++,fortran \
-        --disable-multilib \
-        --program-suffix=-$GCC_VERSION \
-    && make -j4 \
-    && make install-strip \
-    && cd /tmp \
-    && rm -rf /tmp/gcc-$GCC_VERSION /tmp/gcc.tar.gz
 
 bazel_version="3.7.1"
 # Install Bazel version specified in .bazelversion
