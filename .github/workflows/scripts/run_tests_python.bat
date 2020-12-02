bb5e4ce647123d5e79fae38f0fc391b6840b28ca
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-02 10:01:27 +0200

    debug windows

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -1,7 +1,4 @@
 echo on
 
-bazel test --test_output=all --cache_test_results=no //private_set_intersection/python:tests
-
 pip install .
-python private_set_intersection/python/tests.py
-
+bazel test --test_output=all --cache_test_results=no //private_set_intersection/python:tests
