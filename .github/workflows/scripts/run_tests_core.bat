01d8a4b9150715557b2ddc0bdf45596c9e9cb0c3
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 11:52:55 +0200

    update timeouts

--- .github/workflows/scripts/run_tests_core.bat
+++ .github/workflows/scripts/run_tests_core.bat
@@ -1,13 +1,13 @@
 echo on
 
-bazel test --test_output=all //private_set_intersection/cpp:psi_client_test
-bazel test --test_output=all //private_set_intersection/cpp:psi_server_test
-bazel test --test_output=all //private_set_intersection/cpp:bloom_filter_test
-bazel test --test_output=all //private_set_intersection/cpp:gcs_test
-bazel test --test_output=all //private_set_intersection/cpp:golomb_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/cpp:psi_client_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/cpp:psi_server_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/cpp:bloom_filter_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/cpp:gcs_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/cpp:golomb_test
 
-bazel test --test_output=all //private_set_intersection/c:psi_client_test
-bazel test --test_output=all //private_set_intersection/c:psi_server_test
-bazel test --test_output=all //private_set_intersection/c:bloom_filter_test
-bazel test --test_output=all //private_set_intersection/c:gcs_test
-bazel test --test_output=all //private_set_intersection/c:golomb_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/c:psi_client_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/c:psi_server_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/c:bloom_filter_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/c:gcs_test
+bazel test --test_output=all --test_timeout=900 //private_set_intersection/c:golomb_test
