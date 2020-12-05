7da00827f652ec32beaea30a8a6b745ed2901f31
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 13:02:03 +0200

    debug python

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -5,6 +5,7 @@ echo %PYTHONPATH%
 echo %pythonLocation%
 set PATH=%PATH%;%pythonLocation%
 set PYTHONPATH=%pythonLocation%
+set PYTHON_BIN_PATH=%pythonLocation%\python.exe
 
 pip install .
 python private_set_intersection/python/tests.py
