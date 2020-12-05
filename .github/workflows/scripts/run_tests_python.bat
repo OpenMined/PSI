f01b284d990f1838bc8b94479f60039bdd5bf27f
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 12:53:12 +0200

    debug windows

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -3,6 +3,8 @@ echo on
 python -c "import sys; print('\n'.join(sys.path))"
 echo %PYTHONPATH%
 echo %pythonLocation%
+set PATH=%PATH%;%pythonLocation%
+set PYTHONPATH=%pythonLocation%
 
 pip install .
 python private_set_intersection/python/tests.py
