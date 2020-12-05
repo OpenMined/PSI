47c1a017eec9d69d3175c60c0962d0aaa2b47e81
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-05 13:31:44 +0200

    debug windows

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -9,7 +9,5 @@ set PYTHON_BIN_PATH=%pythonLocation%\python.exe
 
 python ./configure.py
 
-bazel test --test_output=all --cache_test_results=no --python_path=%PYTHON_BIN_PATH% //private_set_intersection/python:tests
-
 pip install .
 python private_set_intersection/python/tests.py
