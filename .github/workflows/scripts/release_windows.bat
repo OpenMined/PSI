9a1dfa3f974f4d8c5f33d2b1b02baa65c7906b41
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-08 13:41:47 +0200

    add windows deploy script

--- .github/workflows/scripts/release_windows.bat
+++ .github/workflows/scripts/release_windows.bat
@@ -1,7 +1,6 @@
-echo on
+#!/bin/sh
 
 python -m pip install --upgrade pip
-python -m pip install -r private_set_intersection\python\requirements_dev.txt 
 pip install setuptools wheel twine auditwheel
 
 pip wheel . -w wheel/
