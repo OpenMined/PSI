9ee88434be2733a6e8e73bf53b3f915ce7fe2162
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-08 09:45:40 +0200

    update python env

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -6,7 +6,17 @@ echo %pythonLocation%
 dir %pythonLocation%\
 
 set PATH=%PATH%;%pythonLocation%
-set PYTHONPATH=%pythonLocation%
-set PYTHON_BIN_PATH=%pythonLocation%\python.exe
+set PYTHON_PATH=%pythonLocation%
+
+SET PYTHONHOME=%PYTHON_PATH%
+SET PYTHONPATH=%PYTHON_PATH%
+SET PYTHONEXECUTABLE=%PYTHON_PATH%\python.exe
+SET PYTHONWEXECUTABLE=%PYTHON_PATH%\pythonw.exe
+
+SET PYTHON_EXECUTABLE=%PYTHON_PATH%\python.exe
+SET PYTHONW_EXECUTABLE=%PYTHON_PATH%\pythonw.exe
+SET PYTHON_BIN_PATH=%PYTHON_EXECUTABLE%
+SET PYTHON_LIB_PATH=%PYTHON_PATH%\Lib\site-packages
+SET PATH=%PYTHON_PATH%;%PYTHON_PATH%\Scripts;%DP0%\CMake\Bin;%PATH%
 
 bazel.exe test --test_output=all --cache_test_results=no --test_timeout=900 //private_set_intersection/python:tests
