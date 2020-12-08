17cdd2e8b0ab87a17cd84dcf3b7dbd34f6e30fed
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-08 11:31:52 +0200

    debug windws

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -2,21 +2,22 @@ echo on
 
 python -m pip install --upgrade pip
 
-echo %pythonLocation%
-dir %pythonLocation%\
-
 set PATH=%PATH%;%pythonLocation%
 set PYTHON_PATH=%pythonLocation%
 
+echo %PYTHON_PATH%
+set PYTHON_PATH=!PYTHON_PATH:\=%\\%!
+echo %PYTHON_PATH%
+
 SET PYTHONHOME=%PYTHON_PATH%
 SET PYTHONPATH=%PYTHON_PATH%
-SET PYTHONEXECUTABLE=%PYTHON_PATH%\python.exe
-SET PYTHONWEXECUTABLE=%PYTHON_PATH%\pythonw.exe
+SET PYTHONEXECUTABLE=%PYTHON_PATH%\\python.exe
+SET PYTHONWEXECUTABLE=%PYTHON_PATH%\\pythonw.exe
 
-SET PYTHON_EXECUTABLE=%PYTHON_PATH%\python.exe
-SET PYTHONW_EXECUTABLE=%PYTHON_PATH%\pythonw.exe
+SET PYTHON_EXECUTABLE=%PYTHONEXECUTABLE%
+SET PYTHONW_EXECUTABLE=%PYTHONWEXECUTABLE%
 SET PYTHON_BIN_PATH=%PYTHON_EXECUTABLE%
-SET PYTHON_LIB_PATH=%PYTHON_PATH%\Lib\site-packages
-SET PATH=%PYTHON_PATH%;%PYTHON_PATH%\Scripts;%DP0%\CMake\Bin;%PATH%
+SET PYTHON_LIB_PATH=%PYTHON_PATH%\\Lib\\site-packages
+SET PATH=%PYTHON_PATH%;%PYTHON_PATH%\\Scripts;%DP0%\\CMake\\Bin;%PATH%
 
 bazel.exe test --test_output=all --cache_test_results=no --test_timeout=900 --python_path=%pythonLocation% //private_set_intersection/python:tests
