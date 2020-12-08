3cb25add75cc41999a327f4dd8e87cf5a3bb7f34
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-08 08:36:21 +0200

    debug windows

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -2,4 +2,4 @@ echo on
 
 python -m pip install --upgrade pip
 
-bazel test --test_output=all --cache_test_results=no --test_timeout=900 //private_set_intersection/python:tests
+bazel.exe test --test_output=all --cache_test_results=no --test_timeout=900 //private_set_intersection/python:tests
