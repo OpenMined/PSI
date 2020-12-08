7aa6d5436bf487769273bcae527cb537e8820b29
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-08 09:11:24 +0200

    debug windows

--- .github/workflows/scripts/run_tests_python.bat
+++ .github/workflows/scripts/run_tests_python.bat
@@ -2,4 +2,10 @@ echo on
 
 python -m pip install --upgrade pip
 
+echo %PYTHONPATH%
+echo %pythonLocation%
+set PATH=%PATH%;%pythonLocation%
+set PYTHONPATH=%pythonLocation%
+set PYTHON_BIN_PATH=%pythonLocation%\python.exe
+
 bazel.exe test --test_output=all --cache_test_results=no --test_timeout=900 //private_set_intersection/python:tests
