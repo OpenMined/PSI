2b2d81375eb79554331eac6fdef1a11a1150a978
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-12 22:22:42 +0200

    macos 10.13

--- .github/workflows/scripts/release_osx.sh
+++ .github/workflows/scripts/release_osx.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-export MACOSX_DEPLOYMENT_TARGET=10.15
+export MACOSX_DEPLOYMENT_TARGET=10.13
 
 python -m pip install --upgrade pip
 pip install -r private_set_intersection/python/requirements_dev.txt
 pip install setuptools wheel twine auditwheel
 
-python3 setup.py build bdist_wheel --plat-name macosx_10_15_x86_64 --dist-dir wheel
+python3 setup.py build bdist_wheel --plat-name macosx_10_13_x86_64 --dist-dir wheel
 twine upload --skip-existing wheel/*
