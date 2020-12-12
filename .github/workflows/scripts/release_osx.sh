8796e7823a0297f459b86307ad569b31bbd10497
Cebere Bogdan <bogdan.cebere@gmail.com>
2020-12-12 21:00:43 +0200

    debug upload

--- .github/workflows/scripts/release_osx.sh
+++ .github/workflows/scripts/release_osx.sh
@@ -4,7 +4,8 @@ python -m pip install --upgrade pip
 pip install -r private_set_intersection/python/requirements_dev.txt
 pip install setuptools wheel twine auditwheel
 
-python setup.py bdist_wheel --plat-name=macosx_10_14_x86_64
+python setup.pyi build bdist_wheel --plat-name=macosx_10_14_x86_64
 
 #pip wheel . -w wheel/
 #twine upload --skip-existing wheel/*
+twine upload dist/*.whl --skip-existing
