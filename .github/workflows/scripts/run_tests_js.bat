1c5e659cb0f45c9c8c37ade7e84a52a451e7d5cb
Nick Angelou <angelou.nick@gmail.com>
2022-12-12 00:50:38 -0600

    update gha

--- .github/workflows/scripts/run_tests_js.bat
+++ .github/workflows/scripts/run_tests_js.bat
@@ -1,9 +1,7 @@
 echo on
 
-npm run submodule:update
-npm run em:update
-npm run em:init
 npm install
 npm run build
 npm run build:proto
+npm run compile
 npm run test
--- .github/workflows/scripts/run_tests_js.sh
+++ .github/workflows/scripts/run_tests_js.sh
@@ -7,10 +7,8 @@ then
 fi
 
 # JavaScript
-npm run submodule:update
-npm run em:update
-npm run em:init
 npm install
 npm run build
 npm run build:proto
+npm run compile
 npm run test
