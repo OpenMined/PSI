3aec1ddf17ac9c03c2b1187a9dc372351aafa4c3
Nick Angelou <angelou.nick@gmail.com>
2022-12-20 21:08:10 -0600

    hotfix: pip deps for publishing

--- .github/workflows/scripts/publish_python.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-
-python -m pip install --upgrade pip
-pip install twine packaging
-
-# upload to PyPI
-twine upload --skip-existing bazel-bin/private_set_intersection/python/openmined.psi-*
