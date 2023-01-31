f73522d9da57cde1c15ef546d27f6d32d5d942f8
Nick Angelou <angelou.nick@gmail.com>
2023-01-31 15:12:46 -0600

    update CI CD

--- .github/workflows/scripts/run_tests_python.sh
+++ .github/workflows/scripts/run_tests_python.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 set -e
 
-python -m pip install --upgrade pip
 pip install -r private_set_intersection/python/requirements.txt
 
 # Python + Bazel
 bazel test -c opt --test_output=all //private_set_intersection/python:tests
-
-# Python package
-# pip install .
-# python private_set_intersection/python/tests.py
